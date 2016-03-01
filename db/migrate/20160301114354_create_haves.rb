ef0c5574f8e6b13c98f9c67281ee0c476fde762b
pomutemu <pomutemu@users.noreply.github.com>
2016-03-01 23:15:31 +0900

    have_and_want_btn

---
+++ db/migrate/20160301114354_create_haves.rb
@@ -0,0 +1,13 @@
+class CreateHaves < ActiveRecord::Migration
+  def change
+    create_table :haves do |t|
+      t.references :user, index: true
+      t.references :item, index: true
+      t.string :type
+
+      t.timestamps null: false
+
+      t.index [:user_id, :item_id , :type], unique: true
+    end
+  end
+end
